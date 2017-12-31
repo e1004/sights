62714ad232b1d74bbdd31832d17f9a1c068444df
e1004 <34979322+e1004@users.noreply.github.com>
2017-12-31 12:13:06 +0200

    Andmed strings.xml failist
    .... toomkiriku näitel

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vaatamisväärsuste rakendus
+
+...
+
+## Allikad
+
+* Tartu toomkiriku teksti allikas: https://www.puhkaeestis.ee/et/tartu-ulikooli-muuseumi-toomkiriku-tornid
--- app/src/main/java/example/com/sights/dummy/DummyContent.java
+++ app/src/main/java/example/com/sights/dummy/DummyContent.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import example.com.sights.R;
+
 /**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
@@ -26,10 +28,9 @@ public class DummyContent {
     private static final int COUNT = 25;
 
     static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
+        DummyItem toomkirik = new DummyItem("12",
+                R.string.toomkirik_pealkiri, R.string.toomkirik_tekst);
+        addItem(toomkirik);
     }
 
     private static void addItem(DummyItem item) {
@@ -37,9 +38,6 @@ public class DummyContent {
         ITEM_MAP.put(item.id, item);
     }
 
-    private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
 
     private static String makeDetails(int position) {
         StringBuilder builder = new StringBuilder();
@@ -55,18 +53,14 @@ public class DummyContent {
      */
     public static class DummyItem {
         public final String id;
-        public final String content;
-        public final String details;
+        public final int content;
+        public final int details;
 
-        public DummyItem(String id, String content, String details) {
+        public DummyItem(String id, int content, int details) {
             this.id = id;
             this.content = content;
             this.details = details;
         }
 
-        @Override
-        public String toString() {
-            return content;
-        }
     }
 }
